b11e08b7b7412822348c235ff130d206afe574ac
Sosha Happel <soshappel@gmail.com>
2012-01-12 11:03:28 +0100

    2 data files: GPS van 344 in 07-2010 en alle GPS van 344, 355, 373

---
+++ tools/matlab/makeVectors.m
@@ -0,0 +1,17 @@
+function out = makeVectors(coordinates)
+    % Function that creates vectors from gps coordinates and time stamps
+    % Input: n*4 matrix with : [time_stamp x y z]
+    % Output: Matrix with 3d vectors [vec1 ; vec2 ; ...; vec_n]
+    % The length of the vectors represent the speed, the direction represents the
+    % change of direction 
+    place1 = coordinates(1:size(coordinates)(1)-1, 2:4);
+    place2 = coordinates(2:size(coordinates)(1), 2:4);
+    time1 = coordinates(1:size(coordinates)(1)-1, 1);
+    time2 = coordinates(2:size(coordinates)(1), 1);
+    
+    for i = 1:size(place1)(2)
+        out(:,i) = (place1(:,i) - place2(:,i)) ./ ((time2 - time1)/10000);
+    end 
+
+
+
