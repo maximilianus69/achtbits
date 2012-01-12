0d074f052263988de67d5366d134398a01130111
Inja <Inja@localhost>
2012-01-12 16:28:12 +0100

    working RewriteTime.java (this is now extremely incorrectly named as it is used to rewrite .csv files

--- tools/RewriteTime.java
+++ tools/RewriteTime.java
@@ -5,12 +5,19 @@ import java.text.SimpleDateFormat;
 
 /** Rewrites the date in a .csv file to a timestamp instead of a string
 * and writes it to a new .csv file. 
+* 
+* .csv file line beforehand:
+* "device_info_serial","date_time","latitude","longitude","altitude","pressure","temperature","h_accuracy","v_accuracy","x_speed","y_speed","z_speed","gps_fixtime","location","userflag","satellites_used","positiondop","speed_accuracy"
+*
+* Can include \N (where no data)>
+*
 * @author Maarten Inja*/
 class RewriteTime
 {
     // Example of a date time 2010-07-01 10:01:03 (which results in the following format:) 
     private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    private static String delimiter = ",";
+    private static String inputDelimiter = ",";
+    private static String outputDelimiter = ",";
 
 
     public static void main (String args[])
@@ -27,11 +34,24 @@ class RewriteTime
         ArrayList<String> newLines = new ArrayList<String> ();
         ArrayList<String> lines = readFile(inputFileName); 
         int count = 0;
+        int slashNCount = 0;
         for (String line: lines)
         {
+            // removing lines with \N
+            if (line.indexOf("\\N") != -1)
+            {       
+                slashNCount ++;
+                // by not processing a line with includes '\N' we exclude it in the new file
+                continue;
+            }
+
+            // removing '"' characters
+            //line.replace("\"", "");
+            line = removeChar(line, '\"');
+
             try
             {
-                String[] splittedLine = line.split(delimiter);
+                String[] splittedLine = line.split(inputDelimiter);
                 Date date = simpleDateFormat.parse(splittedLine[1]);
                 splittedLine[1] = String.valueOf(date.getTime());
                 newLines.add(arrayToString(splittedLine)); 
@@ -41,7 +61,9 @@ class RewriteTime
                 count ++;
             }
         }
-        System.out.println(count + " line(s) not rewritten");
+        System.out.println(lines.size() + " line(s) in input file");
+        System.out.println(count + " line(s) not rewritten because of error (the first line?)");
+        System.out.println(slashNCount + " line(s) not included because of occurence of '\\N'");
         writeFile(newLines, outputFileName); 
         System.out.println("done");        
     }
@@ -52,7 +74,7 @@ class RewriteTime
     {
         String line = "";
         for (int i = 0; i <= list.length - 2; i ++)
-            line += list[i] + ";";
+            line += list[i] + outputDelimiter;
         return line + list[list.length - 1] + "\n";
     }
 
@@ -96,5 +118,19 @@ class RewriteTime
           }
         return lines;
     }
+
+    /** Because Java's inbuilt String.'replace' doesnt work :@ */
+    public static String removeChar(String s, char c) 
+    {
+        String r = "";
+        for (int i = 0; i < s.length(); i ++) 
+           if (s.charAt(i) != c) 
+                r += s.charAt(i);
+        return r;
+    }
+
 }
+
+
+
 
