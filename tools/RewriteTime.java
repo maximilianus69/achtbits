0abb4bbc3426ea3f354c9dad30ad70be1cb204f9
Inja <Inja@localhost>
2012-01-12 10:53:02 +0100

    dunno exactly

--- tools/RewriteTime.java
+++ tools/RewriteTime.java
@@ -10,6 +10,7 @@ class RewriteTime
 {
     // Example of a date time 2010-07-01 10:01:03 (which results in the following format:) 
     private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static String delimiter = ",";
 
 
     public static void main (String args[])
@@ -30,7 +31,7 @@ class RewriteTime
         {
             try
             {
-                String[] splittedLine = line.split(";");
+                String[] splittedLine = line.split(delimiter);
                 Date date = simpleDateFormat.parse(splittedLine[1]);
                 splittedLine[1] = String.valueOf(date.getTime());
                 newLines.add(arrayToString(splittedLine)); 
@@ -50,7 +51,7 @@ class RewriteTime
     public static String arrayToString(String[] list)
     {
         String line = "";
-        for (int i = 0; i < list.length - 2; i ++)
+        for (int i = 0; i <= list.length - 2; i ++)
             line += list[i] + ";";
         return line + list[list.length - 1] + "\n";
     }
