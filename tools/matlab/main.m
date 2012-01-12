918576c0c740897d019877a018d2db2831072f83
Inja <Inja@localhost>
2012-01-12 15:36:28 +0100

    removed thousands of .csv files

---
+++ tools/matlab/main.m
@@ -0,0 +1,8 @@
+function out = main(fileName)
+    % Wrapper for reading gps coordinates from a csv file, taking the ones above
+    % the north sea, and calculating vectors with the direction and speed
+    % It uses readgps and makeVectors
+    gps = readgps(fileName);
+    out = makeVectors(gps);
+
+    
