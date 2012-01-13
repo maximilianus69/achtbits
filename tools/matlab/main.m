31f5ebc244cb16c29945280871391160625cbb76
Waard <Waard@localhost>
2012-01-13 11:49:48 +0100

    editing main.m and removed some lines in the derivative plot

--- tools/matlab/main.m
+++ tools/matlab/main.m
@@ -2,7 +2,13 @@ function out = main(fileName)
     % Wrapper for reading gps coordinates from a csv file, taking the ones above
     % the north sea, and calculating vectors with the direction and speed
     % It uses readgps and makeVectors
-    gps = readgps(fileName);
+    Gps = readgps(fileName);
+    input = ones(size(Gps, 1), 5);
+    % ID and Timestamp
+    input(:, 1:2) = Gps(:, 1:2);
+    % X_speed, Y_speed and Z_speed
+    input(:, 3:5) = Gps(:, 6:8);
+    Der = derivative(Gps); 
     out = makeVectors(gps);
 
     
--- tools/matlab/plot/plotSecondDerivative.m
+++ tools/matlab/plot/plotSecondDerivative.m
@@ -1,14 +1,11 @@
-function result = plotSecondDerivative(Matrix, name)
-% plots the second derivative of the 
-% tuple {id, time, x, y, z, xSpeed, ySpeed, zSpeed
-%
+function result = plotSecondDerivative(Matrix)
 % Argument Matrix is an N x 3 matrix containing vectors>
 % N is the number of datapoints. 
 %
 % Time is on the x-axis of the plot. The length of each vector is on the
 % y-axis.
 
-N = size(Matrix)(1);
+N = size(Matrix, 1);
 
 x = [1 : N];
 y = zeros(1, N);
@@ -21,9 +18,9 @@ end
 % (we should create one function that plots everything
 h = subplot(1, 1, 1);
 
-plot(x, y, '-s','LineWidth',1,
-                'MarkerEdgeColor','k',
-                'MarkerFaceColor','auto',
+plot(x, y, '-s','LineWidth',1, ...
+                'MarkerEdgeColor','k', ...
+                'MarkerFaceColor','auto', ...
                 'MarkerSize',13); 
 
 xlabel('index');
