1f18d71d3a58cbfea49d4a04698a08f02c79f467
Waard <Waard@localhost>
2012-01-13 15:12:19 +0100

    findClusters now finds 1 point per peak

---
+++ tools/matlab/main.m
@@ -0,0 +1,26 @@
+function out = main(fileName)
+    % Wrapper for reading gps coordinates from a csv file, taking the ones above
+    % the north sea, and calculating vectors with the direction and speed
+    % It uses readgps and makeVectors
+    addpath('plot');
+    Gps = readgps(fileName);
+    Input = ones(size(Gps, 1), 5);
+    % ID and Timestamp
+    Input(:, 1:2) = Gps(:, 1:2);
+    % X_speed, Y_speed and Z_speed
+    Input(:, 3:5) = Gps(:, 6:8);
+    Der = derivative(Input);
+    figure(1);
+    subplot(2,1,1);
+    plotSecondDerivative(Gps(:, 6:8));
+    subplot(2,1,2);
+    plotSecondDerivative(Der(:, 3:5));
+    peakThres = 2*10^(-5);
+    Clusters = findClusters(Gps, peakThres);
+    % 16 minutes = 960 000 ms
+    %Clusters = awesomizeClusters(Clusters, 96000);
+
+
+    out = Clusters .- Gps(1, 2);
+
+    
