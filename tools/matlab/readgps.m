f8f007ccd910e5a5d5df30afe8a99034d5d80df4
Inja <Inja@localhost>
2012-01-13 10:42:55 +0100

    removed a strange .csv file and fixed rewritetime even though i did not change anything

---
+++ tools/matlab/readgps.m
@@ -0,0 +1,19 @@
+function matrix = readgps(fileName)
+    % Function that reads gps data from a csv file seperated by ;
+    % The seventh and ninth column are now assumed to contain the gps data (x, y, z)
+    % The second column contains a time stamp
+    % Returns: A size(input(1), 4) by 4 matrix with time stamps and gps data
+    A = dlmread(fileName, ';');
+    matrix = ones(size(A,1), 4);
+    matrix(:, 1) = A(:, 2);
+    % In case we have seperated values:
+    matrix(:, 2:4) = A(:,3:5);
+    northsea = dlmread('noordzeeCoordinates.txt',',');
+    inNorthSea = inpolygon(matrix(:,2), matrix(:,3), northsea(:,2), northsea(:,1));
+    %inNorthSea = ones(size(matrix,1),1);
+    for i = 1:size(matrix,2)
+        matrix(:,i) = inNorthSea .* matrix(:,i);
+    end 
+    
+    % Remove zero rows
+    matrix(~any(matrix,2),:) = [];
