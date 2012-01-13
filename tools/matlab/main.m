77e8b95c3586c58e59c444c3a1e772310517bc05
Waard <Waard@localhost>
2012-01-13 12:03:57 +0100

    works?

--- tools/matlab/main.m
+++ tools/matlab/main.m
@@ -2,13 +2,14 @@ function out = main(fileName)
     % Wrapper for reading gps coordinates from a csv file, taking the ones above
     % the north sea, and calculating vectors with the direction and speed
     % It uses readgps and makeVectors
+    addpath('plot');
     Gps = readgps(fileName);
-    input = ones(size(Gps, 1), 5);
+    Input = ones(size(Gps, 1), 5);
     % ID and Timestamp
-    input(:, 1:2) = Gps(:, 1:2);
+    Input(:, 1:2) = Gps(:, 1:2);
     % X_speed, Y_speed and Z_speed
-    input(:, 3:5) = Gps(:, 6:8);
-    Der = derivative(Gps); 
-    out = makeVectors(gps);
+    Input(:, 3:5) = Gps(:, 6:8);
+    Der = derivative(Input); 
+    plotSecondDerivative(Der);
 
     
