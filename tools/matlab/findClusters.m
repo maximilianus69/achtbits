c3d5f59aa05add0e57679c2838483b9e67a5da54
Waard <Waard@localhost>
2012-01-16 13:59:29 +0100

    editing findClusters.m

---
+++ tools/matlab/findClusters.m
@@ -0,0 +1,57 @@
+function Clusters = findClusters( Data, peakThres)
+%FINDCLUSTERS Finds the start and end point of a behaviour period
+%   Input:
+%   Data = ID, time, x, y, z, speed_x, speed_y, speed_z
+%   peakThres = threshold for recognising peaks
+%   
+%   output:
+%   Clusters = nx2 matrix with start/end times 
+%   [begin_t1 begin_t1; ... ; begin_tn begin_tn]
+%
+%   use the data to calulate acceleration
+%   find big chances in acceleration with threshold
+%   use these points to determine start and end time of a period
+
+% find the acceleration
+DataPrep = [Data(:, 1:2) Data(:, 6:8)];
+Acc = derivative(DataPrep);
+
+% get length of acceleration vectors
+AccRight = Acc(:, 3:5)
+
+for i = 1:size(AccRight, 1)
+    Acc2(i, 1) = norm(AccRight(i, :));
+end
+% find index of peaks by comparing with the threshold
+Peaks = Acc2 > peakThres
+% Get the middle of the peak:
+% TODO: WIthout loop?
+beginPos = 0;
+endPos = 0;
+for j = 1:size(Peaks, 1)
+    if Peaks(j) == 1 && beginPos == 0
+        beginPos = j;
+    elseif Peaks(j) == 0 && beginPos ~= 0
+        Peaks(beginPos+1:(j-1)) = 0;
+        % Let the 1-value be on the middle of the peak
+        %Peaks(round((beginPos+(j-1))/2)) = 1;
+        Peaks(j) = 1;
+        beginPos = 0;
+    end
+end
+        
+        
+        
+
+
+        
+
+PeakPos = find(Peaks);
+
+% use indices to retrieve start-time and end-time of period
+Clusters = [];
+time = Data(:, 2);
+for i = 1:(size(PeakPos, 1)-1)
+    Clusters = [Clusters; time(i) time(i+1)];
+end
+
---
+++ tools/matlab/main.m
@@ -0,0 +1,33 @@
+function out = main(fileName)
+    % Wrapper for reading gps coordinates from a csv file, taking the ones above
+    % the north sea, and calculating vectors with the direction and speed
+    % It uses readgps and makeVectors
+    addpath('plot');
+    Gps = readgps(fileName);
+    Input = ones(size(Gps, 1), 5);
+    % ID and Timestamp
+    Input(:, 1:2) = Gps(:, 1:2);
+    % X_speed, Y_speed and Z_speed
+    Input(:, 3:5) = Gps(:, 6:8);
+    Der = derivative(Input);
+    
+    % Threshold based on different time stamps
+    %peakThres = 2*10^(-5);
+    peakThres = 0.01;
+    Clusters = findClusters(Gps, peakThres)
+    
+    figure(1);
+    subplot(2,1,1);
+    plotSecondDerivative(Input(:, 3:5), Input(:, 2));
+    subplot(2,1,2);
+    % 16 minutes = 960 000 ms
+    %Clusters = awesomizeClusters(Clusters, 400);
+    plotSecondDerivative(Der(:, 3:5), Der(:, 2), [Clusters(:,1); Clusters(:,2)]);
+    Gps(1, 2)
+    Gps(size(Gps, 1), 2)
+
+
+
+    out = Clusters;
+
+    
