8a09384c839b02db665f93c0fd9ff2456894740f
jesse <jesse@localhost>
2012-01-16 16:28:49 +0100

    update createClusterFeatures

---
+++ tools/matlab/getTimeAndSpeed.m
@@ -0,0 +1,8 @@
+function TimeAndSpeed = getTimeAndSpeed( Data )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+TimeAndSpeed = [Data(:, 2) Data(:, 6:7)];
+
+end
+
