9f44c56c0e300aac3757fa77a2b0e14abd8478eb
Waard <Waard@localhost>
2012-01-16 11:21:00 +0100

    changed readgps.m to read the new data files (index 6:8 for speed)

---
+++ tools/matlab/main.m
@@ -0,0 +1,29 @@
+function out = main(fileName)
+    % Wrapper for reading gps coordinates from a csv file, taking the ones above
+    % the north sea, and calculating vectors with the direction and speed
+    % It uses readgps and makeVectors
+    addpath('plot');
+    Gps = readgps(fileName);
+    Input = ones(size(Gps, 1), 5);
+    % ID and Timestamp
+    Input(:, 1:2) = Gps(:, 1:2);
+    % X_speed, Y_speed and Z_speed
+    Input(:, 3:5) = Gps(:, 6:8);
+    Der = derivative(Input);
+
+    peakThres = 2*10^(-5);
+    Clusters = findClusters(Gps, peakThres)
+    
+    figure(1);
+    subplot(2,1,1);
+    plotSecondDerivative(Input(:, 3:5), Input(:, 2));
+    subplot(2,1,2);
+    plotSecondDerivative(Der(:, 3:5), Der(:, 2), [Clusters(:,1); Clusters(:,2)]);
+    
+    % 16 minutes = 960 000 ms
+    %Clusters = awesomizeClusters(Clusters, 96000);
+
+
+    out = Clusters - Gps(1, 2);
+
+    
---
+++ tools/matlab/readgps.m
@@ -0,0 +1,28 @@
+function Matrix = readgps(fileName)
+    % Function that reads gps data from a csv file seperated by ;
+    % The seventh and ninth column are now assumed to contain the gps data (x, y, z)
+    % The second column contains a time stamp
+    % Returns: A size(input(1), 4) by 4 matrix with time stamps and gps data
+    % "device_info_serial","date_time","latitude","longitude","altitude","pressure","temperature","h_accuracy","v_accuracy","x_speed","y_speed","z_speed","gps_fixtime","location","userflag","satellites_used","positiondop","speed_accuracy"
+    Data = dlmread(fileName, ',');
+    Matrix = ones(size(Data, 1), 8);
+    % The bird ID
+    Matrix(:, 1) = Data(:, 1);
+    % The time stamp
+    Matrix(:, 2) = Data(:, 2);
+    % In case we have seperated values:
+    % The x, y and z position
+    Matrix(:, 3:5) = Data(:,3:5);
+    % The x_speed, y_speed, z_speed
+    Matrix(:, 6:8) = Data(:, 6:8);
+    northsea = dlmread('noordzeeCoordinates.txt',',');
+    inNorthSea = inpolygon(Matrix(:,3), Matrix(:,4), northsea(:,1), northsea(:,2));
+    %inNorthSea = ones(size(Matrix, 1),1);
+    %remove zero values:
+    Matrix(~any(Matrix,2),:) = [];
+
+    for i = 1:size(Matrix, 2)
+        Matrix(:,i) = inNorthSea .* Matrix(:,i);
+    end 
+    Matrix(~any(Matrix,2),:) = [];
+
