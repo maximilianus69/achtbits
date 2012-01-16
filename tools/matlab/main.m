29a9e4c17972bcfd6b232e217aa1eb5b3729922f
jesse <jesse@localhost>
2012-01-16 16:27:01 +0100

    update createClusterFeatures

---
+++ tools/matlab/main.m
@@ -0,0 +1,26 @@
+function out = main(fileName)
+    % Wrapper for reading gps coordinates from a csv file, taking the ones above
+    % the north sea, and calculating vectors with the direction and speed
+    % It uses readgps and makeVectors
+    addpath('plot');
+    Gps = readgps(fileName);
+    Input = ones(size(Gps, 1), 5);
+    % ID and Timestamp
+    Input(:, 1:2) = Gps(:, 1:2);
+    % X_speed, Y_speed and Z_speed
+    Input(:, 3:5) = Gps(:, 6:8);
+    Der = derivative(Input);
+    
+    % Threshold based on different time stamps
+    %peakThres = 2*10^(-5);
+    peakThres = 0.01;
+    % Clusters = findClusters(Gps, peakThres)
+    Clusters = findClusters(Gps(:, 2), Der(:, 3:5), peakThres)
+    
+    figure(1);
+    subplot(2,1,1);
+    plotSecondDerivative(Input(:, 3:5), Input(:, 2));
+    subplot(2,1,2);
+    Clusters = awesomizeClusters(Clusters, 1500);
+    plotSecondDerivative(Der(:, 3:5), Der(:, 2), [Clusters(:,1); Clusters(:,2)]);
+    out = Clusters;
