fad090ee6431138c7e31f67fb563de1b0b1e2232
Waard <Waard@localhost>
2012-01-16 15:12:33 +0100

    find clusters now works

--- tools/matlab/findClusters.m
+++ tools/matlab/findClusters.m
@@ -1,57 +1,29 @@
-function Clusters = findClusters( Data, peakThres)
-%FINDCLUSTERS Finds the start and end point of a behaviour period
-%   Input:
-%   Data = ID, time, x, y, z, speed_x, speed_y, speed_z
-%   peakThres = threshold for recognising peaks
-%   
-%   output:
-%   Clusters = nx2 matrix with start/end times 
-%   [begin_t1 begin_t1; ... ; begin_tn begin_tn]
-%
-%   use the data to calulate acceleration
-%   find big chances in acceleration with threshold
-%   use these points to determine start and end time of a period
-
-% find the acceleration
-DataPrep = [Data(:, 1:2) Data(:, 6:8)];
-Acc = derivative(DataPrep);
-
-% get length of acceleration vectors
-AccRight = Acc(:, 3:5)
-
-for i = 1:size(AccRight, 1)
-    Acc2(i, 1) = norm(AccRight(i, :));
-end
-% find index of peaks by comparing with the threshold
-Peaks = Acc2 > peakThres
-% Get the middle of the peak:
-% TODO: WIthout loop?
-beginPos = 0;
-endPos = 0;
-for j = 1:size(Peaks, 1)
-    if Peaks(j) == 1 && beginPos == 0
-        beginPos = j
-    elseif Peaks(j) == 0 && beginPos ~= 0
-        Peaks(beginPos+1:(j-1)) = 0
-        % Let the 1-value be on the middle of the peak
-        %Peaks(round((beginPos+(j-1))/2)) = 1;
-        Peaks(j) = 1
-        beginPos = 0
+function Clusters = findClusters(Time, Derivative, peakThres)
+    % Accepts a derivative with time stamps in the first column
+    %Normals = ones(size(Derivative, 1), 1);
+        
+    % Create length by 1 array of normals
+    for i = 1:size(Derivative, 1)
+        Normals(i, 1) = norm(Derivative(i, :));
     end
-end
-        
-        
-        
+    
+    Peaks = Normals > peakThres;
+    NewPeaks = ones(size(Peaks));
 
+    for i = 1:(size(Peaks, 1)-1)
+        if(Peaks(i) == 1 && Peaks(i+1) == 0)
+            NewPeaks(i) = 1;
+        elseif(Peaks(i) == 0 && Peaks(i+1))
+            NewPeaks(i) = 1;
+        else
+            NewPeaks(i) = 0;
+        end
+    end
 
-        
-
-PeakPos = find(Peaks);
-
-% use indices to retrieve start-time and end-time of period
+PeakPos = find(NewPeaks);
 Clusters = [];
-time = Data(:, 2);
 for i = 1:(size(PeakPos, 1)-1)
-    Clusters = [Clusters; time(i) time(i+1)];
+    Clusters = [Clusters; Time(PeakPos(i)) Time(PeakPos(i+1))];
 end
+
 
