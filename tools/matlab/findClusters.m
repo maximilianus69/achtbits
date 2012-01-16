73ed19347d2d343870a4c03fb6795cdebd03e0e3
Waard <Waard@localhost>
2012-01-16 15:05:53 +0100

    Find clusters fixed a bit

--- tools/matlab/findClusters.m
+++ tools/matlab/findClusters.m
@@ -30,13 +30,13 @@ beginPos = 0;
 endPos = 0;
 for j = 1:size(Peaks, 1)
     if Peaks(j) == 1 && beginPos == 0
-        beginPos = j;
+        beginPos = j
     elseif Peaks(j) == 0 && beginPos ~= 0
-        Peaks(beginPos+1:(j-1)) = 0;
+        Peaks(beginPos+1:(j-1)) = 0
         % Let the 1-value be on the middle of the peak
         %Peaks(round((beginPos+(j-1))/2)) = 1;
-        Peaks(j) = 1;
-        beginPos = 0;
+        Peaks(j) = 1
+        beginPos = 0
     end
 end
         
