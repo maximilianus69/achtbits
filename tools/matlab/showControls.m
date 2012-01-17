eccd8ac98aea4093ee50fc6e3d2d55db1fbc8b53
jesse <jesse@localhost>
2012-01-17 15:58:45 +0100

    feature fixes

--- tools/matlab/showControls.m
+++ tools/matlab/showControls.m
@@ -11,14 +11,14 @@ N = length(Buttons);
 buttonH = 60;
 
 close all;
-figid = figure('Position', [300 300 300 buttonH*N]);
+figid = figure('Position', [600 200 400 buttonH*N], 'color', 'white');
 hold on;
 axis([0 10 0 N]);
 axis off;
 
 for i = 1:N
 	line(xlim, [i i]);
-	text(0, i-0.3, Buttons(i), 'fontsize', 20, 'backgroundcolor','red');
+	text(0, i-0.3, Buttons(i), 'fontsize', 20);
 end
 
 [ix, iy] = ginput(1);
