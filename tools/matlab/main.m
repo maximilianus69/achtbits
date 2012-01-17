90ccf616386a58aaae4f3f205f9d4f845a82d17c
Inja <Inja@localhost>
2012-01-17 14:05:00 +0000

    main.m altered

--- tools/matlab/main.m
+++ tools/matlab/main.m
@@ -2,6 +2,16 @@ function [Gps Clusters] = main(fileName)
     % Wrapper for reading gps coordinates from a csv file, taking the ones above
     % the north sea, and calculating vectors with the direction and speed
     % It uses readgps and makeVectors
+
+    % Threshold based on different time stamps, used for findClusters, determines
+    % what datapoints becomes peaks?
+    %peakThres = 2*10^(-5);
+    peakThres = 0.01;
+
+    % Threshold used for awesomizeClusters, determines peaks in a range become one? 
+    timeThres = 500;
+
+
     addpath('plot');
     Gps = readgps(fileName);
     Input = getTimeAndSpeed(Gps);
@@ -17,9 +27,6 @@ function [Gps Clusters] = main(fileName)
     time = Der(:, 1);
     Derivative = Der(:, 2:3);
 
-    % Threshold based on different time stamps
-    %peakThres = 2*10^(-5);
-    peakThres = 0.01;
     Clusters = findClusters(time, Derivative, peakThres);
 
     % plot all the clusters
@@ -28,7 +35,7 @@ function [Gps Clusters] = main(fileName)
         [Clusters(:, 1); Clusters(:, 2)]);
 
     % group sequences of small clusters into bigger ones
-    Clusters = awesomizeClusters(Clusters, 2000);
+    Clusters = awesomizeClusters(Clusters, timeThres);
     
     % plot the new clusters
     subplot(3,1,3);
