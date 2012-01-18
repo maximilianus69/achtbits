9517624f0d7ae590d8c7fb54f461ad59f94a5459
jesse <jesse@localhost>
2012-01-18 15:15:13 +0100

    added noord-holland plot without zooming problem

--- tools/matlab/plotTrajectory.m
+++ tools/matlab/plotTrajectory.m
@@ -12,12 +12,13 @@ sessionY = SessionCoordinates(:, 2);
 clusterX = ClusterCoordinates(:, 1);
 clusterY = ClusterCoordinates(:, 2);
 
-%geoshow(gca, 'nh_zh_shape.shp');
 hold('on')
+axis([min(sessionY)-0.05 max(sessionY)+0.05 min(sessionX)-0.05 max(sessionX)+0.05]);
 
+geoshow(gca, 'plot/nh_zh_shape/nh_zh_shape.shp');
 geoshow(gca, sessionX, sessionY, 'color', 'b');
-
 geoshow(gca, clusterX, clusterY, 'color', 'r');
+
 hold('off')
 
 end
