df398b65eea5c642223262f5d4612a35513b3406
jesse <jesse@localhost>
2012-01-18 11:42:27 +0100

    exapmle of annotate ui

--- tools/matlab/plotFeatureInfo.m
+++ tools/matlab/plotFeatureInfo.m
@@ -6,7 +6,7 @@ function plotFeatureInfo( Feature )
 %[startTime, endTime, duration, avgSpeed, heightDiff, grndDist]
        
 figure('color', 'white', 'position', [340 400 400 200]);
-axis([0 6 6 10]);
+axis([0 6 5 10]);
 axis off;
 
 set(0, 'DefaultTextHorizontalAlignment', 'right');
@@ -17,6 +17,7 @@ text(1, 9, 'Duration');
 text(1, 8, 'Avg speed');
 text(1, 7, 'Height diff');
 text(1, 6, 'Abs distance');
+text(1, 5, 'Tot distance');
 
 
 set(0, 'DefaultTextHorizontalAlignment', 'left');
@@ -25,7 +26,8 @@ text(2, 10, '1'); % cluster
 text(2, 9, strcat(num2str(Feature(3)), ' sec')); % duration
 text(2, 8, strcat(num2str(Feature(4)), ' m/s')); % speed
 text(2, 7, strcat(num2str(Feature(5)), ' m')); % height diff
-text(2, 6, strcat(num2str(Feature(6)), ' km'));; % abs distance
+text(2, 6, strcat(num2str(Feature(6)), ' km')); % abs distance
+text(2, 5, strcat(num2str(Feature(7)), ' km')); % tot distance
 
 text(4, 9, strcat(num2str(Feature(3)/3600), ' hr')); % duration
 text(4, 8, strcat(num2str(Feature(4)*3.6), ' km/h')); % speed
