dca902889dbd4c869b48b3491fc7338b91cd7534
Sosha Happel <soshappel@gmail.com>
2012-01-19 16:38:24 +0100

    added annotateBird.m (starts annotating sessions of bird and writes results to file)

--- tools/matlab/showControls.m
+++ tools/matlab/showControls.m
@@ -15,7 +15,7 @@ axis off;
 
 for i = 1:N
 	line(xlim, [i i]);
-	text(0, i-0.5, strcat(num2str(i), ')  ',Buttons(i)), 'fontsize', 13);
+	text(0, i-0.4, strcat(num2str(i), ')  ',Buttons(i)), 'fontsize', 16);
 end
 
 [ix, iy] = ginput(1);
