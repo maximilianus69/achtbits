90b97529b5e03a235219344e69f3f7bde886d44b
Waard <Waard@localhost>
2012-01-19 10:57:30 +0100

    extra awesome clusters

--- tools/matlab/awesomizeClusters.m
+++ tools/matlab/awesomizeClusters.m
@@ -5,7 +5,6 @@ function AwesomeClusters = awesomizeClusters(LameClusters, timeThreshold)
     % Output: Same as input, but with fewer clusters.
     Diff = LameClusters(:, 2) - LameClusters(:, 1)
     DiffBool = Diff > timeThreshold
-    begun = 0;
     j = 1;
     AwesomeClusters(1, 1) = LameClusters(1, 1);
     array = (1:size(Diff, 1)-1);
@@ -14,16 +13,20 @@ function AwesomeClusters = awesomizeClusters(LameClusters, timeThreshold)
         % The difference boolean = 1 and the previous is = 0
         if (DiffBool(i) && ~DiffBool(i+1))
             %Set current begin time for this cluster
-            AwesomeClusters(j, 2) = LameClusters(i, 1)
+            AwesomeClusters(j, 2) = LameClusters(i, 2);
             j = j+1;
-            AwesomeClusters(j, 1) = LameClusters(i, 1)
-            begun = 1;
+            AwesomeClusters(j, 1) = LameClusters(i, 2);
         %if this cluster ends
         elseif (~DiffBool(i) && DiffBool(i+1))
-            begun = 0;
-            AwesomeClusters(j, 2) = LameClusters(i+1, 2)
+            % Add the cluster end
+            AwesomeClusters(j, 2) = LameClusters(i, 2);
             j = j + 1;
-            AwesomeClusters(j, 1) = LameClusters(i+1, 2)
+            % Add the cluster between the chaos clusters
+            AwesomeClusters(j, 1) = LameClusters(i, 2);
+            AwesomeClusters(j, 2) = LameClusters(i+1, 2);
+            j = j+1;
+            % Add the new cluster beginning
+            AwesomeClusters(j, 1) = LameClusters(i+1, 2);
             if(i < size(array, 2))
                 array(i+1) = [];
             end
@@ -33,6 +36,6 @@ function AwesomeClusters = awesomizeClusters(LameClusters, timeThreshold)
     D = AwesomeClusters(:, 2) - AwesomeClusters(:, 1);
     DB = D > 0;
     AwesomeClusters = AwesomeClusters .* [DB DB];
-    AwesomeClusters(~any(AwesomeClusters,2),:) = []
+    AwesomeClusters(~any(AwesomeClusters,2),:) = [];
    
 
