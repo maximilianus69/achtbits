b211ef30d4caa2283ff540495c3dbebb4ccb2b0a
Sosha Happel <soshappel@gmail.com>
2012-01-20 11:30:39 +0100

    now actually added annotateBird.m

---
+++ tools/matlab/annotateBird.m
@@ -0,0 +1,63 @@
+function [ output_args ] = annotateBird( deviceId, startSession )
+%ANNOTATEBIRD starts annotation of all sessions of given device
+%   INPUT:
+%   deviceId - ID number of the device as a string
+%   startSession(optional, default:0) - ID of starting session, integer 
+%   
+%   checks if there are sessions for this device
+%   loops through all sessions and calls annotateSession
+%   writes to file:
+%   sessionData - data of every data point in session from readgps
+%   path: tools/deviceDEVICEID/session_SESSIONID/...
+%       device_DEVICEID_session_SESSIONID_sessiondata.csv
+%   
+%   clusterFeatures - features and class per cluster (row)
+%   path: tools/deviceDEVICEID/session_SESSIONID/...
+%       device_DEVICEID_session_SESSIONID_clusterFeatures.csv
+
+if nargin < 2
+    sessionId = 0;
+else
+    sessionId = startSession;
+end
+
+folderPath = strcat('..\parsedCsvFiles\device', deviceId);
+type = exist(folderPath);
+
+% check if there is data for this bird ID
+if type  == 7
+    % open an annotation folder for bird
+    % for each session in the folder:
+    %   - open an annotation folder for session
+    %   - run annotateSession
+    
+    outputDeviceFolder = strcat('..\annotatedData\device', deviceId);
+    
+    if exist(outputDeviceFolder, 'dir') ~= 7
+        mkdir(outputDeviceFolder);
+    end
+    
+    inputFilePrefix = strcat('\device_', deviceId, '_gps_session_');
+    
+    sessionFilePath = strcat(folderPath, inputFilePrefix, sprintf('%03d', sessionId), '.csv');
+    
+    while exist(sessionFilePath) == 2
+        
+        sessionName = strcat('session_', sprintf('%03d', sessionId));
+        fileName = strcat('\device_', deviceId, '_', sessionName);
+        outputPath = strcat(outputDeviceFolder, '\', sessionName);
+    
+        if exist(outputPath, 'dir') ~= 7
+            mkdir(outputPath);
+        end
+    
+        annotateSession(deviceId, sessionId, sessionFilePath, outputPath, fileName);
+        
+        sessionId = sessionId + 1;
+        
+        sessionFilePath = strcat(folderPath, inputFilePrefix, sprintf('%03d', sessionId), '.csv');
+    end
+end
+
+end
+
