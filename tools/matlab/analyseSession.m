6558e9c251affb98a8e5a46e7fdee3ffba74f26e
Waard <Waard@localhost>
2012-01-23 11:39:53 +0100

    last cluster now has points until the very last

--- tools/matlab/analyseSession.m
+++ tools/matlab/analyseSession.m
@@ -2,12 +2,16 @@ function Clusters = analyseSession( SessionData )
 %ANALYSESESSION Finds the clusters in a session 
 %   calculates the acceleration and uses this to determine cluster
 %   start/end
+%   output:
+%   Clusters = nx2 matrix with start/end times 
+%   [begin_t1 begin_t1; ... ; begin_tn begin_tn]
 
 % *** maybe add more session analysis?***
 
 
 % get time and speed
 time = SessionData(:, 2);
+lastTime = time(size(time, 1), :)
 Speed = SessionData(:, 6:7);
 
 % find the acceleration
@@ -16,11 +20,12 @@ Der = derivative([time Speed]);
 Derivative = Der(:, 2:3);
 
 % Threshold based on different time stamps
-%peakThres = 2*10^(-5);
 peakThres = 0.015;
 
 % find all threshold crossings
 Clusters = findClusters(time, Derivative, peakThres);
+Clusters(size(Clusters, 1), 2) = time(size(time, 1))
+lastTimeClusters = Clusters(size(Clusters, 1), :)
 
 % group sequences of small clusters into bigger ones
 Clusters = awesomizeClusters(Clusters, 1200);
