5e8d9a3ba8366a480b91abbe51980a685920e35a
jesse <jesse@localhost>
2012-01-24 16:20:47 +0100

    added speed vector plot

--- tools/matlab/plotFeatureInfo.m
+++ tools/matlab/plotFeatureInfo.m
@@ -5,7 +5,7 @@ function plotFeatureInfo( Feature, classes )
 % Feature format:
 %[startTime, endTime, duration, avgSpeed, heightDiff, grndDist]
        
-axis([0 6 5 10]);
+axis([0 6 4 10]);
 axis off;
 
 set(0, 'DefaultTextHorizontalAlignment', 'left');
@@ -18,7 +18,8 @@ text(1, 8, 'Avg vel.');
 text(1, 7, 'Y diff');
 text(1, 6, 'Abs dist');
 text(1, 5, 'Tot dist');
-text(1, 4, 'Prev.');
+text(1, 4, 'Angle var');
+text(1, 3, 'Prev.');
 
 text(3, 10, num2str(Feature(1))); % cluster
 %text(2, 9, strcat(num2str(Feature(4)), ' sec')); % duration
@@ -26,7 +27,8 @@ text(3, 10, num2str(Feature(1))); % cluster
 text(3, 7, strcat(num2str(Feature(6)), ' m')); % height diff
 text(3, 6, strcat(num2str(Feature(7)), ' km')); % abs distance
 text(3, 5, strcat(num2str(Feature(8)), ' km')); % tot distance
-text(3, 4, classes(Feature(end)));
+text(3, 4, strcat(num2str(Feature(9)), ' rads')); % angle variance
+text(3, 3, classes(Feature(end)));
 
 text(3, 9, secs2hms(Feature(4))); % duration
 text(3, 8, strcat(num2str(Feature(5)*3.6), ' km/h')); % speed
