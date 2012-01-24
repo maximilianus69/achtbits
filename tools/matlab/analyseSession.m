dd33ca81e03e5ca24440295fd2274eaea21ee607
jesse <jesse@localhost>
2012-01-24 11:26:56 +0100

    removed derivative error

---
+++ tools/matlab/analyseSession.m
@@ -0,0 +1,31 @@
+function Clusters = analyseSession( SessionData )
+%ANALYSESESSION Finds the clusters in a session 
+%   calculates the acceleration and uses this to determine cluster
+%   start/end
+%   output:
+%   Clusters = nx2 matrix with start/end times 
+%   [begin_t1 begin_t1; ... ; begin_tn begin_tn]
+
+% *** maybe add more session analysis?***
+
+
+% get time and speed
+time = SessionData(:, 2);
+Speed = SessionData(:, 6:7);
+
+% find the acceleration
+Der = derivative([time Speed]);
+
+Derivative = Der(:, 2:3);
+
+% Threshold based on different time stamps
+peakThres = 5;
+
+% find all threshold crossings
+Clusters = findClusters(time, Derivative, peakThres);
+
+% group sequences of small clusters into bigger ones
+Clusters = awesomizeClusters(Clusters, 1200);
+
+end
+
---
+++ tools/matlab/plotClusterAcc.m
@@ -0,0 +1,42 @@
+function [ output_args ] = plotClusterAcc( clusterTime, SessionAccData )
+%PLOTCLUSTERACC plots accelerometer data for cluster
+%   Detailed explanation goes here
+
+% finds all timestamps in SessionAccDatam also in cluster 
+SessionAccTime = SessionAccData(:, 2);
+afterStartCluster = SessionAccTime >= clusterTime(1);
+beforeEndCluster = SessionAccTime <= clusterTime(2);
+inCluster = afterStartCluster & beforeEndCluster;
+
+if sum(inCluster) > 0
+
+    % get the data that is in cluster
+    ClusterAccData = SessionAccData(inCluster, 2:6);
+
+    % get all time-stamps and find the middle one
+    timeEntries = unique(ClusterAccData(:, 1));
+    % amountOfEntries = size(timeEntries, 1);
+
+    timeToShow = timeEntries(round(size(timeEntries, 1)/2));
+    isEntryToShow = ClusterAccData == timeToShow; 
+
+    % get the data of the correct time stamp
+    entriesToShow = ClusterAccData(isEntryToShow, 2:5);
+
+    % plot the data
+    x = entriesToShow(:, 1);
+
+    hold('on')
+    plot(x, entriesToShow(:, 2), 'color', 'red')
+
+    plot(x, entriesToShow(:, 3), 'color', 'green')
+
+    plot(x, entriesToShow(:, 4), 'color', 'blue')
+
+    title('accelerometer');
+
+    hold('off')
+end
+
+end
+
