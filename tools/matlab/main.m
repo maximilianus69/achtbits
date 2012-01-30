35d8864d6c4ec3ac8210744a98be61548a2144c7
Waard <Waard@localhost>
2012-01-30 12:38:38 +0100

    better plot

---
+++ tools/matlab/main.m
@@ -0,0 +1,49 @@
+function [Gps Clusters] = main(deviceId, sessionId)
+    % Wrapper for reading gps coordinates from a csv file, taking the ones above
+    % the north sea, and calculating vectors with the direction and speed
+    % It uses readgps and makeVectors
+
+
+    histogramSizeSeconds = 900;
+    timestampStep = 100;
+
+    addpath('plot');
+    Gps = readgps(deviceId, sessionId);
+    Input = getTimeAndSpeed(Gps);
+    
+    % plot the speed
+    fig = figure(1);
+    %set(fig,'units','normalized','outerposition',[0 0 1 1]);
+    
+    
+    % Calculate the current speed from x_speed and y_speed
+    for i = 1:size(Input, 1)
+        Speed(i, :) = norm(Input(i, 2:3));
+    end
+    subplot(3,1,1);
+    % plotSecondDerivative('velocity', Input(:, 2:3), Input(:, 1));
+    plotSecondDerivative2('velocity', Speed, Input(:,1));
+    
+    %Der = derivative(Input);
+    % split Der in time and derivative
+    %time = Der(:, 1);
+    %Derivative = Der(:, 2);
+
+    [NewTime, NewSpeed] = interpolate(Input(:, 1), Speed, timestampStep, 'pchip');
+
+    subplot(3, 1, 2);
+    plotSecondDerivative('Interpolated', NewSpeed, NewTime);
+   
+    [m1Course, m2Course, m3Course, timestamps] = histogramCompare(NewTime, NewSpeed, histogramSizeSeconds, timestampStep);
+
+    timestamps = (timestamps - timestamps(1) + histogramSizeSeconds/2) ./ 60;
+    
+    subplot(3, 1, 3);
+    % To plot the right width
+    NewTime = (NewTime - NewTime(1)) ./ 60;
+    plot(NewTime, NewSpeed, 'color', 'w'); 
+    hold on
+    % Plot the actual values
+    plotHists(m1Course, m2Course, m3Course, timestamps);
+    hold off
+
