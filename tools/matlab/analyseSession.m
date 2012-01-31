a95d73c335852fcec241c3e1b51d6f3b4a4888ff
Waard <Waard@localhost>
2012-01-31 14:33:59 +0100

    new analyseSession, with hopefully better clusters

--- tools/matlab/analyseSession.m
+++ tools/matlab/analyseSession.m
@@ -1,33 +1,45 @@
 function Clusters = analyseSession( SessionData )
-%ANALYSESESSION Finds the clusters in a session 
-%   calculates the acceleration and uses this to determine cluster
-%   start/end
-%   output:
-%   Clusters = nx2 matrix with start/end times 
-%   [begin_t1 begin_t1; ... ; begin_tn begin_tn]
+    %ANALYSESESSION Finds the clusters in a session 
+    %   calculates the acceleration and uses this to determine cluster
+    %   start/end
+    %   output:
+    %   Clusters = nx2 matrix with start/end times 
+    %   [begin_t1 begin_t1; ... ; begin_tn begin_tn]
 
-% *** maybe add more session analysis?***
+    % *** maybe add more session analysis?***
 
-% Threshold based on different time stamps
-peakThres = 5;
-% Threshold for time between peaks
-timeThreshold = 1200;
+    histogramSizeSeconds = 900;
+    timestampStep = 150;
+    % Initialize the half of the window. In the loop we will look back and forth by this size
+    halfWindowSize = 3;
+    
+    % Get the time and instantaneous speed
+    Time = SessionData(:, 2);
+    InstSpeed = SessionData(:, 9:10);
 
-% get time and speed
-time = SessionData(:, 2);
-Speed = SessionData(:, 6:7);
+    % Calculate the speed
+    for i = 1:size(Input, 1)
+        Speed(i, :) = norm(SessionData(i, 2:3));
+    end
+ 
+    % Get interpolated values
+    [NewTime, NewSpeed] = interpolate(Input(:, 1), Speed, timestampStep, 'pchip');
+    [NewXTime, NewXSpeed] = interpolate(Input(:, 1), abs(InstSpeed(:, 1)), timestampStep, 'pchip');
+    [NewYTime, NewYSpeed] = interpolate(Input(:, 1), abs(InstSpeed(:, 2)), timestampStep, 'pchip');
+ 
+    % Get the histogram awesomeness values
+    [m1Course, m2Course, m3Course, timestamps] = histogramCompare(NewTime, NewSpeed,NewXSpeed, NewYSpeed, histogramSizeSeconds, timestampStep);
+    
+    % Find the max values for which behavior is done. This defines clusters
+    [Max Index] = max([m1Course m2Course m3Course]');
+    % Class will be a N by 1 vector containing the classes
+    Class = [];
+    Class(:, 1)= timestamps(halfWindowSize:size(timestamps, 1)-halfWindowSize- 1) + histogramSizeSeconds/2; 
 
-% find the acceleration
-Der = derivative([time Speed]);
-
-Derivative = Der(:, 2);
-
-
-% find all threshold crossings
-Clusters = findClusters(time, Derivative, peakThres);
-
-% group sequences of small clusters into bigger ones
-Clusters = awesomizeClusters(Clusters, timeThreshold);
-
-end
-
+    % Smooth the clusters.
+    for i = halfWindowSize + 1:size(Index, 2) - halfWindowSize
+        Class(i-halfWindowSize, 2) = mode(Index(i-halfWindowSize:i+halfWindowSize));
+    end
+    
+    % find the cluster edges
+    Clusters = simpleFindClusters(Class, 1200);
