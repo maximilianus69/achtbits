fb80e392b2ca6ef3de6f07fd0ca21342d2cbfb05
Waard <Waard@localhost>
2012-01-31 14:13:20 +0100

    better clusters from the main version

--- tools/matlab/testClustering.m
+++ tools/matlab/testClustering.m
@@ -1,11 +1,15 @@
-function result = testClustering()
+function result = testClustering(session)
 % Runs 'main.m' a couple of times with different values that are known
 % to be troublesome cases. Every exception should be shown with those
 % values. 
 
 % 9, 11, 12 
 
-for x = 1:15
+if(nargin < 1)
+    session = 0;
+end
+
+for x = session:15
     main('344', x);
     uiwait;
 end
