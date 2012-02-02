ee742315950a7a78c4de75be6808b3ac72130543
Inja <Inja@localhost>
2012-02-02 13:50:56 +0100

    correct jar file

--- tools/java/Wrapper.java
+++ tools/java/Wrapper.java
@@ -7,18 +7,27 @@ public class Wrapper
     327, 329, 344, 355, 373, 533, 534, 535, 536, 537, 538, 540, 541,
     542};
 
-    //public static final String OUTPUT_DIR = ".." + Io.SYSTEM_SEPARATOR + "parsedCsvFiles";
-    public static final String OUTPUT_DIR = "temp";
-    public static final String DIR_TO_DATA_FILES = ".." + Io.SYSTEM_SEPARATOR + 
+    //public static final String outputDir = ".." + Io.SYSTEM_SEPARATOR + "parsedCsvFiles";
+    public static String outputDir = "temp";
+    public static String dirToDataFiles = ".." + Io.SYSTEM_SEPARATOR + 
         ".." + Io.SYSTEM_SEPARATOR + "rawdata" + Io.SYSTEM_SEPARATOR;
 
     public static void main(String args[])
     {
 
+        if (args.length != 2)
+        {
+            System.out.println("Run this with two command line arguments: 1st the output directory, 2nd the directory of the data files.");
+            System.out.println("Will now exit");
+            return; 
+        }
+        outputDir = args[0];
+        dirToDataFiles = args[1];
+
         for (int id : KNOWN_IDS)
         {
-            File gpsCsvFile = new File(DIR_TO_DATA_FILES + "DATA_ALL_GPS_BIRD_" + id + ".csv");
-            File accelCsvFile = new File(DIR_TO_DATA_FILES + "DATA_ALL_ACCELEROMETER_BIRD_" + id + ".csv");
+            File gpsCsvFile = new File(dirToDataFiles + "DATA_ALL_GPS_BIRD_" + id + ".csv");
+            File accelCsvFile = new File(dirToDataFiles + "DATA_ALL_ACCELEROMETER_BIRD_" + id + ".csv");
 
             if (gpsCsvFile.exists())
             {
@@ -27,17 +36,17 @@ public class Wrapper
                 {
                     System.out.println(" with " + accelCsvFile.getName());
                     args = new String[3];
-                    args[0] = DIR_TO_DATA_FILES + gpsCsvFile.getName();
-                    args[1] = OUTPUT_DIR;
-                    args[2] = DIR_TO_DATA_FILES + accelCsvFile.getName();
+                    args[0] = dirToDataFiles + gpsCsvFile.getName();
+                    args[1] = outputDir;
+                    args[2] = dirToDataFiles + accelCsvFile.getName();
                     PreprocessCsvFiles.main(args);
                 }
                 else
                 {
                     System.out.println(" without accel file");
                     args = new String[2];
-                    args[0] = DIR_TO_DATA_FILES + gpsCsvFile.getName();
-                    args[1] = OUTPUT_DIR;
+                    args[0] = dirToDataFiles + gpsCsvFile.getName();
+                    args[1] = outputDir;
                     PreprocessCsvFiles.main(args);
                 }
             }
