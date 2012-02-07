98b3e357ac5a3f3c3d27adfcb189567e07dc8b5b
Waard <Waard@localhost>
2012-02-07 21:57:10 +0100

    added report to the report title page, and user guide to the user guide title page

--- tools/matlab/interpolatedToRealTimestamp.m
+++ tools/matlab/interpolatedToRealTimestamp.m
@@ -1,4 +1,9 @@
 function NewClusters = interpolatedToRealTimestamp(Clusters, realTimestamps)
+    % INTERPOLATEDTOREALTIMESTAMP: returns the timestamps as they are in the original session data
+    % Input: Clusters, realTimestamps
+    % Output: NewClusters, an array consisting of the clusters that are put in,
+    % but with timestamps that are in the original data, so not in the
+    % interpolated data.
     NewClusters = [];
     for i = 1:size(Clusters, 2)
         for j = 1:size(Clusters, 1)
