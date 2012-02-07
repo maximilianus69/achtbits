6ace078436090f179df739196f538341ea3f7545
Waard <Waard@localhost>
2012-02-07 22:05:58 +0100

    done commenting my code

--- tools/matlab/fourierOnAcc.m
+++ tools/matlab/fourierOnAcc.m
@@ -11,15 +11,6 @@ function frequency = fourierOnAcc(AccData, timeStamp)
     
     AccData = AccData(find(AccData(:, 2) ==  timeStamp), :);
 
-    %{ Delete this if you would want a plot (TODO: Remove this before delivery)
-    if 0
-    % Plot the original
-    subplot(2, 1, 1);
-    plotAcc(AccData, timeStamp);
-    title('The original accellerometer data');
-    end
-    %}
-
     AccDataRaw = AccData(:, 4:6);
 
     % From the matlab example: (http://www.mathworks.nl/help/techdoc/ref/fft.html)
@@ -37,37 +28,17 @@ function frequency = fourierOnAcc(AccData, timeStamp)
     GoodY1 = 2.*abs(Y1(1:NFFT./2+1));
     GoodY2= 2.*abs(Y2(1:NFFT./2+1));
     GoodY3 = 2.*abs(Y3(1:NFFT./2+1));
-    %{ For plotting. TODO: Remove this when we are sure it works
-    if 0
-    subplot(2, 1, 2);
-    plot(f,GoodY1);
-    hold on
-    plot(f, GoodY1, '-s','LineWidth',1, ...
-                'MarkerEdgeColor','r', ...
-                'MarkerFaceColor','auto', ...
-                'MarkerSize',4); 
-    plot(f, GoodY2, '-s','LineWidth',1, ...
-                'MarkerEdgeColor','g', ...
-                'MarkerFaceColor','auto', ...
-                'MarkerSize',4); 
-    plot(f, GoodY3, '-s','LineWidth',1, ...
-                'MarkerEdgeColor','b', ...
-                'MarkerFaceColor','auto', ...
-                'MarkerSize',4); 
-    title('Single-Sided Amplitude Spectrum of Accellerometer Data');
-    xlabel('Frequency (Hz)');
-    ylabel('|AccDataRaw(f)|');
-    end
-    %}
+    
     % Find the peaks in the fourier 
     [values freq1] = findpeaks(GoodY1, 'MINPEAKHEIGHT', 0.2);
     [values freq2] = findpeaks(GoodY2, 'MINPEAKHEIGHT', 0.2);
     [values freq3] = findpeaks(GoodY3, 'MINPEAKHEIGHT', 0.2);
+
     % We need vertical vectors
     freq1 = freq1';
     freq2 = freq2';
     freq3 = freq3';
-    %sizes = [size(freq1, 1) size(freq2, 1) size(freq3, 1)];
+    
     % Create the output matrix
     frequency = zeros(1, 3);
     if(~isempty(freq1))
